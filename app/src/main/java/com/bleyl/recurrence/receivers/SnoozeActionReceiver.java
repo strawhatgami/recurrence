18e1c21b7acd260833b63224cfbed63a3aa3dbfd
Anis <sajed.anis@free.fr>
2018-05-27 19:09:38 +0200

    allow to not being asked for a custom duration when snoozing

--- app/src/main/java/com/bleyl/recurrence/receivers/SnoozeActionReceiver.java
+++ app/src/main/java/com/bleyl/recurrence/receivers/SnoozeActionReceiver.java
@@ -5,8 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.preference.PreferenceManager;
 
+import com.bleyl.recurrence.R;
 import com.bleyl.recurrence.activities.SnoozeDialogActivity;
 import com.bleyl.recurrence.utils.AlarmUtil;
+import com.bleyl.recurrence.utils.NotificationUtil;
+
+import java.util.Calendar;
 
 public class SnoozeActionReceiver extends BroadcastReceiver {
 
@@ -23,9 +27,23 @@ public class SnoozeActionReceiver extends BroadcastReceiver {
         Intent closeIntent = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         context.sendBroadcast(closeIntent);
 
-        Intent snoozeIntent = new Intent(context, SnoozeDialogActivity.class);
-        snoozeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        snoozeIntent.putExtra("REMINDER_ID", reminderId);
-        context.startActivity(snoozeIntent);
+        if (PreferenceManager.getDefaultSharedPreferences(context).getBoolean("checkBoxHideSnoozeActivity", false)) {
+            int nagMinutes = PreferenceManager.getDefaultSharedPreferences(context)
+                .getInt("nagMinutes", context.getResources().getInteger(R.integer.default_nag_minutes));
+            int nagSeconds = PreferenceManager.getDefaultSharedPreferences(context)
+                .getInt("nagSeconds", context.getResources().getInteger(R.integer.default_nag_seconds));
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.SECOND, nagSeconds);
+            calendar.add(Calendar.MINUTE, nagMinutes);
+            Intent alarmIntent = new Intent(context.getApplicationContext(), SnoozeReceiver.class);
+            AlarmUtil.setAlarm(context.getApplicationContext(), alarmIntent, reminderId, calendar);
+            NotificationUtil.cancelNotification(context.getApplicationContext(), reminderId);
+        } else {
+            Intent snoozeIntent = new Intent(context, SnoozeDialogActivity.class);
+            snoozeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            snoozeIntent.putExtra("REMINDER_ID", reminderId);
+            context.startActivity(snoozeIntent);
+        }
     }
 }
